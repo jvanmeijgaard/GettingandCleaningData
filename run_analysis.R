2b6820ffedf8831f99c0bb199c298dc6e09fd558
Jeroen van Meijgaard <jeroen@cerebroinc.com>
2017-05-21 15:16:17 -0700

    Add code to obtain descriptives for codebook

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,7 @@
 library(plyr)
+library(dplyr)
+
+options(width=105)
 
 # Read in feature names list
 df.featurenames <- read.table('./data/UCI HAR Dataset/features.txt', col.names=c('variable','featurelabel'))
@@ -14,11 +17,11 @@ df.xtrain <- read.table('./data/UCI HAR Dataset/train/X_train.txt', numerals = "
 df.ytrain <- read.table('./data/UCI HAR Dataset/train/y_train.txt', colClasses="factor", col.names='activity')
 
 df.xtest <- read.table('./data/UCI HAR Dataset/test/X_test.txt', numerals = "no.loss", colClasses="numeric", col.names=df.featurenames$featurelabel)
-df.ytest <- read.table('./data/UCI HAR Dataset/test/y_test.txt', colClasses="factor", col.names='activity')
+df.ytest <- read.table('./data/UCI HAR Dataset/test/y_test.txt', colClasses="integer", col.names='activity')
 
 # Read in subject ids for both sets
 df.subjecttrain <- read.table('./data/UCI HAR Dataset/train/subject_train.txt', colClasses="factor", col.names='subject')
-df.subjecttest <- read.table('./data/UCI HAR Dataset/test/subject_test.txt', colClasses="factor", col.names='subject')
+df.subjecttest <- read.table('./data/UCI HAR Dataset/test/subject_test.txt', colClasses="integer", col.names='subject')
 
 # Combine training data into 1 data frame, select mean and std variables and label 'training' to column 'source'
 df.train <- cbind(df.xtrain[,mnstdindicator], df.ytrain, df.subjecttrain)
@@ -44,3 +47,17 @@ names(df.allmeans) <- c('activity','subject',paste0('Avg',names(df.all[, 1:sum(m
 # Write out data sets as csv files
 write.csv(df.all,file='./GettingandCleaningData/MnStdFeatures.csv', row.names = FALSE, fileEncoding = 'UTF-8')
 write.csv(df.allmeans,file='./GettingandCleaningData/MnStdFeaturesAverages.csv', row.names = FALSE, fileEncoding = 'UTF-8')
+
+# Tables with counts of observation by activity and by subject to include in CodeBook.md
+pander(table(df.all$activity))
+pander(table(df.all$subject))
+
+# Create data frame for mean of feature data to include in CodeBook.md
+featuremeans <- as.data.frame(cbind(colMeans(df.all[,1:66]),apply(df.all[,1:66],2,sd),apply(df.all[,1:66],2,min),apply(df.all[,1:66],2,max)))
+names(featuremeans) <- c('mean','std','min','max')
+featuremeans
+
+# Create data frame for mean of averaged feature data to include in CodeBook.md
+avgfeaturemeans <- as.data.frame(cbind(colMeans(df.allmeans[,3:68]),apply(df.allmeans[,3:68],2,sd),apply(df.allmeans[,3:68],2,min),apply(df.allmeans[,3:68],2,max)))
+names(avgfeaturemeans) <- c('mean','std','min','max')
+avgfeaturemeans
